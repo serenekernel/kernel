cd9b7be5a4be217f264257a90c13bea562740319
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-03-18 21:40:04 +1100

    fix: dpcs executing after unlocking dpc_queue in irq

--- kernel/src/common/irql.c
+++ kernel/src/common/irql.c
@@ -22,9 +22,14 @@ irql_t irql_lower(irql_t new_level) {
     irql_t old_irql = _arch_irql_get();
     assertf(old_irql >= new_level, "IRQL lowered below new level: old=%d new=%d", old_irql, new_level);
 
-    if(new_level <= IRQL_DISPATCH && !dpc_queue_empty()) { dpc_execute_all(); }
+    if(old_irql == new_level) { return old_irql; }
 
-    if(old_irql != new_level) { _arch_irql_set(new_level); }
+    if(new_level <= IRQL_DISPATCH && !dpc_queue_empty()) {
+        _arch_irql_set(IRQL_DISPATCH);
+        dpc_execute_all();
+    }
+
+    _arch_irql_set(new_level);
     return old_irql;
 }
 
