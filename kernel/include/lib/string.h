97cf15d8cc6c3c7e423a25392ff0c781fda29b8e
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-03-20 13:47:25 +1100

    minor: remove useless functions

--- kernel/include/lib/string.h
+++ kernel/include/lib/string.h
@@ -11,15 +11,3 @@ void* memmove(void* dest, const void* src, size_t n);
 int memcmp(const void* s1, const void* s2, size_t n);
 int strcmp(const char* s1, const char* s2);
 int strncmp(const char* s1, const char* s2, size_t n);
-
-// copies n pages from userspace src to userspace dest
-// @note: these functions don't do cow copies, nor does it map anything new in, nor do we handle page faults (those shouldn't happen)
-void memcpy_um_um(vm_allocator_t* dest_alloc, vm_allocator_t* src_alloc, virt_addr_t dest, virt_addr_t src, size_t page_count);
-void memcpy_km_um(vm_allocator_t* dest_alloc, virt_addr_t dest, virt_addr_t src, size_t page_count);
-// @todo: unimpl
-void memcpy_um_km(vm_allocator_t* src_alloc, virt_addr_t dest, virt_addr_t src, size_t page_count);
-
-// sets n pages in userspace dest to value c
-void memset_vm(vm_allocator_t* dest_alloc, virt_addr_t dest, int c, size_t page_count);
-
-[[nodiscard]] bool memcpy_um_um_unaligned(vm_allocator_t* dest_alloc, vm_allocator_t* src_alloc, virt_addr_t dest, virt_addr_t src, size_t length);
