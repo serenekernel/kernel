e008693e009d223e005d0cc5a7bafe116f5f8b33
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-03-27 03:06:19 +1100

    minor: rename max_size -> capacity

--- kernel/include/lib/buffer.h
+++ kernel/include/lib/buffer.h
@@ -1,11 +1,12 @@
+#pragma once
 #include <memory/heap.h>
 
 typedef struct {
     uint8_t* data;
     size_t size;
-    size_t max_size;
+    size_t capacity;
 } buffer_t;
 
-buffer_t buffer_create(size_t max_size);
+buffer_t buffer_create(size_t capacity);
 void buffer_free(buffer_t* buffer);
 void buffer_append(buffer_t* buffer, const void* data, size_t size);
--- kernel/src/lib/buffer.c
+++ kernel/src/lib/buffer.c
@@ -2,24 +2,24 @@
 #include <memory/heap.h>
 #include <string.h>
 
-buffer_t buffer_create(size_t max_size) {
+buffer_t buffer_create(size_t capacity) {
     buffer_t buffer = { 0 };
-    buffer.data = heap_alloc(max_size);
-    buffer.max_size = max_size;
+    buffer.data = heap_alloc(capacity);
+    buffer.capacity = capacity;
     return buffer;
 }
 
 void buffer_free(buffer_t* buffer) {
-    heap_free(buffer->data, buffer->max_size);
+    heap_free(buffer->data, buffer->capacity);
     buffer->data = NULL;
     buffer->size = 0;
-    buffer->max_size = 0;
+    buffer->capacity = 0;
 }
 
 void buffer_append(buffer_t* buffer, const void* data, size_t size) {
-    if(buffer->size + size > buffer->max_size) {
-        buffer->data = heap_realloc(buffer->data, buffer->max_size, buffer->max_size * 2);
-        buffer->max_size *= 2;
+    if(buffer->size + size > buffer->capacity) {
+        buffer->data = heap_realloc(buffer->data, buffer->capacity, buffer->capacity * 2);
+        buffer->capacity *= 2;
     }
 
     memcpy(buffer->data + buffer->size, data, size);
